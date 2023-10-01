484fe2ee741d23d6f8b2bf99c2e1ecb7745e2348
phamhoangngocquan2805 <phamhoangngocquan2805@gmail.com>
2023-10-01 16:40:06 +0700

    add bai tap tuan 3

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import FirstScreen from './baiTapTuan03/fisrtScreen';
+import Screen1a from './baiTapTuan03/screen_1a';
+import Screen1b from './baiTapTuan03/screen_1b';
+import Screen1c from './baiTapTuan03/screen_1c';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Screen1a
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
